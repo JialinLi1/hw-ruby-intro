a9f51d35b6e60dfd24422ce9948c3249d6d706b9
JialinLi1 <jli128@tamu.edu>
2022-02-13 22:23:05 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,66 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum arr 
+  res = 0
+  for i in arr do
+      res += i
+  end
+  res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  a,b = 0,0
+  for i in arr do 
+      if i>a 
+          if i>b 
+              a=b
+              b=i
+              next
+          end
+          a=i 
+          next
+      end
+  end
+  a+b
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? arr,n
+  for i in arr do
+      if arr.include?(n-i)
+          return true
+      end
+  end
+  return false
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  (s =~ /^[aeiouAEIOU]/) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  (s =~ /^[01]*00$|^0$/) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  # getter, setter handled by attr_accessor
+  attr_accessor :isbn, :price 
+
+  def initialize isbn, price
+      # doesn't care what's actually in isbn, as long as non-empty, starts with number and includes number/hypens
+      raise ArgumentError if isbn == "" or price <=0 or not isbn =~ /^\d[\d\-]*$/
+      @isbn, @price = isbn, price 
+  end
+
+  def price_as_string
+      "$%0.2f" % @price
+  end
 end
